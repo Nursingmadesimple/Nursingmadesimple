5d99626de55a1066aee7c9e38e75b13a46d2e838
Nursingmadesimple <nursingmadesimple101@gmail.com>
2024-10-20 21:03:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
 
     // Correct answers for each question
     const correctAnswers = {
-        q1: 'C',
-        q2: 'B',
-        q3: 'C',
+        q1: 'B',
+        q2: 'C',
+        q3: 'B',
         q4: 'A',
-        q5: 'C'
+        q5: 'D'
     };
 
     // Check each question
